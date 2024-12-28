80acdfb1fc7771b6fb3e091c03a4037ddb7f7542
Oleh Zelinskyi <blackbard27@yahoo.com>
2024-12-28 20:47:58 +0200

    Responsive for progress bar

--- app/components/progress/index.tsx
+++ app/components/progress/index.tsx
@@ -5,6 +5,8 @@ import { formatCurrency } from "@/app/utils/formatCurrency";
 import clsx from "clsx";
 import { useSelector } from "react-redux";
 
+import Image from "next/image";
+import { useState } from "react";
 import SmallOptionShape from "../option-shape/small-option-shape";
 import styles from "./styles.module.css";
 
@@ -26,26 +28,49 @@ const isPassed = (
 };
 
 const Progress = ({ steps, currency }: ProgressProps) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const currentStep = useSelector((state: RootState) => state.game.currentStep);
 
+  const handleToggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+  };
+
   return (
-    <aside className={styles.container}>
-      {steps.map((step, index) => (
-        <div
-          key={step}
-          className={clsx(styles.option, {
-            [styles.selected]: currentStep === step,
-            [styles.passed]: isPassed(currentStep ?? "", steps, index),
-          })}
-        >
-          <SmallOptionShape className={styles.hexshape}>
-            <div className={styles.label}>
-              {formatCurrency(Number(step), currency)}
-            </div>
-          </SmallOptionShape>
-        </div>
-      ))}
-    </aside>
+    <>
+      <Image
+        className={styles.burger}
+        src="burger.svg"
+        width="13"
+        height="13"
+        alt="open"
+        onClick={handleToggleMenu}
+      />
+      <aside className={clsx(styles.container, menuOpen && styles.visible)}>
+        <Image
+          className={styles.cross}
+          src="cross.svg"
+          width="13"
+          height="13"
+          alt="close"
+          onClick={handleToggleMenu}
+        />
+        {steps.map((step, index) => (
+          <div
+            key={step}
+            className={clsx(styles.option, {
+              [styles.selected]: currentStep === step,
+              [styles.passed]: isPassed(currentStep ?? "", steps, index),
+            })}
+          >
+            <SmallOptionShape className={styles.hexshape}>
+              <div className={styles.label}>
+                {formatCurrency(Number(step), currency)}
+              </div>
+            </SmallOptionShape>
+          </div>
+        ))}
+      </aside>
+    </>
   );
 };
 
